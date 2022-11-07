292e965e1e5fa0e394c56cd335c1c5b78530922e
Weston <westonford@outlook.com>
2022-11-07 10:05:14 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Jumper.Game;
+
+namespace Seeker
+{
+    private class Program
+    {
+        private static void Main(string[] args)
+        {
+            Director director = new Director();
+            director.StartGame();
+        }
+    }
+}
