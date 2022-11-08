d174a47c5087aa84e28837e0d689ef6ca3eac16b
Weston <westonford@outlook.com>
2022-11-08 09:48:37 -0700

    created word.cs

---
+++ Game/Word.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Jumper.Game;
+
+public class Word
+{
+    private List<string> characters = new List<string>();
+    public Word()
+    {}
+
+}
